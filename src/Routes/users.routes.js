9cb801ddbb74a13d584efbd99b8a752bda42467a
Taiane Karine <94652702+taianekarine@users.noreply.github.com>
2022-12-12 17:39:25 -0300

    Update users.routes.js

--- src/Routes/users.routes.js
+++ src/Routes/users.routes.js
@@ -8,14 +8,14 @@ const UserAvatarController = require('../controllers/UserAvatarController');
 const ensureAuthenticated = require('../Middlewares/ensureAuthenticated');
 
 const usersRoutes = Router();
-const uplodad = multer(uploadConfig.MULTER);
+const upload = multer(uploadConfig.MULTER);
 
 const usersController = new UsersController()
 const userAvatarController = new UserAvatarController();
 
 usersRoutes.post('/', usersController.create);
 usersRoutes.put('/', ensureAuthenticated, usersController.update);
-usersRoutes.patch('/avatar', ensureAuthenticated, uplodad.single('avatar'), userAvatarController.update);
+usersRoutes.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update);
 
 
 module.exports = usersRoutes;
